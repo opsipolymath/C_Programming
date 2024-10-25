d5c411f931d15b8031e0dd1c1799aacd0f20a7ce
opsipolymath <opsipolymath+github@gmail.com>
2024-10-24 21:15:14 -0400

    Initial comment for lesson 2
    Comments mastered.

---
+++ 2. Comments and Escapes/HelloWorld.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    // This is a comment.
+
+    /* 
+        This
+        is
+        a
+        multiline
+        comment
+    */
+
+    printf("I like pizza!\n");
+    return 0;
+}
