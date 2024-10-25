847bdd4126da693a72cfec4d0cfcbabae1cb8494
opsipolymath <opsipolymath+github@gmail.com>
2024-10-24 21:21:02 -0400

    Final result of lesson 2; esc sequences mastered

--- 2. Comments and Escapes/HelloWorld.c
+++ 2. Comments and Escapes/HelloWorld.c
@@ -23,7 +23,7 @@ int main() {
             \t = tab
     */
 
-    printf("I like pizza!\n");
+    printf("\'I like Pizza\' \\ Abraham Lincoln (probably)\n\n");
 
     printf("1\t2\t3\n4\t5\t6\n7\t8\t9\n");
     return 0;
