4daf36d556cdd78149148a5a71a6f88cabb2416a
opsipolymath <opsipolymath+github@gmail.com>
2024-10-24 21:19:31 -0400

    First round of escape sequences

--- 2. Comments and Escapes/HelloWorld.c
+++ 2. Comments and Escapes/HelloWorld.c
@@ -11,6 +11,20 @@ int main() {
         comment
     */
 
+    /*
+        Escape sequences:
+            A character combination consisting of a backslash
+            (\) followed by a letter or combination of digits.
+            They specify actions within a line or string of
+            text.
+
+        Examples:
+            \n = newline
+            \t = tab
+    */
+
     printf("I like pizza!\n");
+
+    printf("1\t2\t3\n4\t5\t6\n7\t8\t9\n");
     return 0;
 }
